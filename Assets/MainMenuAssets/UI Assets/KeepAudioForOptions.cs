c8025047ae34a30232c492cc6b5963a5b99c5b07
amwilsons <94584647+amwilsons@users.noreply.github.com>
2023-08-17 22:39:47 -0400

    Added new audio track.

--- Assets/MainMenuAssets/UI Assets/KeepAudioForOptions.cs
+++ Assets/MainMenuAssets/UI Assets/KeepAudioForOptions.cs
@@ -2,10 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class KeepAudioForOptions : MonoBehaviour
+public class DoNotDestroy : MonoBehaviour
 {
-    void Awake()
+    public void Awake()
     {
-        DontDestroyOnLoad(transform.gameObject);
+        GameObject[] musicObj = GameObject.FindGameObjectsWithTag("GameMusic");
+        if (musicObj.Length > 1)
+        {
+            Destroy(this.gameObject);
+        }
+        DontDestroyOnLoad(this.gameObject);
     }
 }

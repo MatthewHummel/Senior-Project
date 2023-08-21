dadab3976a1d6197f866b5c5a902213116cf24b2
MatthewHummel <matthew2hummel@gmail.com>
2023-08-21 14:23:29 -0400

    Comments in generation script. About to try and make new api system

--- Assets/Scripts/MattTextGeneration.cs
+++ Assets/Scripts/MattTextGeneration.cs
@@ -7,58 +7,68 @@ namespace HuggingFace.API.Examples
 {
     public class MattTextGeneration : MonoBehaviour
     {
+        //fields for UI components
         [SerializeField] private ScrollRect scrollRect;
         [SerializeField] private TMP_Text conversationText;
         [SerializeField] private TMP_InputField inputField;
         [SerializeField] private Button sendButton;
         [SerializeField] private Button clearButton;
+        //Text color for the player and bot
         [SerializeField] private Color userTextColor = Color.blue;
         [SerializeField] private Color botTextColor = Color.black;
-
+        //Navigation buttons
         [SerializeField] private Button upButton;
         [SerializeField] private Button downButton;
         [SerializeField] private Button leftButton;
         [SerializeField] private Button rightButton;
 
+        //Unused field that was used to send text to the input field when working on Button Navigation
         //[SerializeField] private TMP_InputField navText;
 
+        //Fields to store the player's name and selected class
         private string classText;
         private string nameText;
 
-
+        //Init task
         private TextGenerationTask textGenerationTask = new TextGenerationTask();
 
-        //private Conversation conversation = new Conversation();
+        //Store color
         private string userColorHex;
         private string botColorHex;
         private string errorColorHex;
+        //Bool used when waiting for a response from the language model
         private bool isWaitingForResponse;
 
+        //on Load
         private void Awake()
         {
+            //set colors. Note that the error color hex is hard-coded. No real reason, Red just works.
             userColorHex = ColorUtility.ToHtmlStringRGB(userTextColor);
             botColorHex = ColorUtility.ToHtmlStringRGB(botTextColor);
             errorColorHex = ColorUtility.ToHtmlStringRGB(Color.red);
 
+            //If we have a selected class, (we always should)
             if (PlayerPrefs.HasKey("SelectedClass"))
             {
+                //Get the string
                 classText = PlayerPrefs.GetString("SelectedClass");
             }
-
+            //Get the name of the player
             nameText = CharacterScene.characterscene.player_name;
-
         }
 
         private void Start()
         {
+            //Listeners for input field, activate the input field.
             sendButton.onClick.AddListener(SendButtonClicked);
             clearButton.onClick.AddListener(ClearButtonClicked);
             inputField.ActivateInputField();
             inputField.onEndEdit.AddListener(OnInputFieldEndEdit);
+            //Sends an inital query to start off the scene.
             SendInitialQuery();
         }
 
-       // attempt to make arrow keys on keyboard work for movement. not working right now. :(
+       //Make arrow keys on keyboard work for movement.
         private void Update()
         {
             if (Input.GetKeyDown(KeyCode.LeftArrow))
@@ -79,38 +89,37 @@ namespace HuggingFace.API.Examples
             }
         }
 
-
+        //When clicking the send button
         private void SendButtonClicked()
         {
             SendQuery();
         }
-
+        //when clicking the up arrow
         public void UpNavButtonClicked()
         {
             //navText.text = "I navigate forward.";
             SendUpQuery();
         }
-
+        //when clicking the down arrow
         public void DownNavButtonClicked()
         {
             //navText.text = "I navigate backwards.";
             SendDownQuery();
         }
-
+        //when clicking the left arrow
         public void LeftNavButtonClicked()
         {
             //navText.text = "I navigate to the left.";
             SendLeftQuery();
         }
-
+        //when clicking the right arrow
         public void RightNavButtonClicked()
         {
             //navText.text = "I navigate to the right.";
             SendRightQuery();
         }
 
-
-
+        //Sends the query if the user clicks enter/return
         private void OnInputFieldEndEdit(string text)
         {
             if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter))
@@ -118,7 +127,6 @@ namespace HuggingFace.API.Examples
                 SendQuery();
             }
         }
-
 
         //method for sending the query to the API
         public void SendQuery()
@@ -187,7 +195,7 @@ namespace HuggingFace.API.Examples
 
         }
 
-
+        //logic for the query sent when using the up arrow
         public void SendUpQuery()
         {
             if (isWaitingForResponse) return;
@@ -250,6 +258,7 @@ namespace HuggingFace.API.Examples
 
         }
 
+        //logic for the query sent when using the down arrow
         public void SendDownQuery()
         {
             if (isWaitingForResponse) return;
@@ -316,6 +325,7 @@ namespace HuggingFace.API.Examples
 
         }
 
+        //logic for the query sent when using the left arrow
         public void SendLeftQuery()
         {
             if (isWaitingForResponse) return;
@@ -382,6 +392,7 @@ namespace HuggingFace.API.Examples
 
         }
 
+        //logic for the query sent when using the right arrow
         public void SendRightQuery()
         {
             if (isWaitingForResponse) return;
